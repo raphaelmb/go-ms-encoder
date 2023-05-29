111741c0ae3bec1e23a9fedd196623fd37de3260
Raphael Berger <raphaelmb@gmail.com>
2023-05-29 17:58:50 -0300

    add video service download

---
+++ application/services/video_service.go
@@ -0,0 +1,59 @@
+package services
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/raphaelmb/go-ms-encoder/application/repositories"
+	"github.com/raphaelmb/go-ms-encoder/domain"
+
+	"cloud.google.com/go/storage"
+)
+
+type VideoService struct {
+	Video           *domain.Video
+	VideoRepository repositories.VideoRepository
+}
+
+func NewVideoService() VideoService {
+	return VideoService{}
+}
+
+func (v *VideoService) Download(bucketName string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	bkt := client.Bucket(bucketName)
+	obj := bkt.Object(v.Video.FilePath)
+
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(os.Getenv("localStoragePath") + "/" + v.Video.ID + ".mp4")
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(body)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	log.Printf("video %v has been stored", v.Video.ID)
+
+	return nil
+}
