8800140b2001c7c667514cf50ccc643388e4a389
Raphael Berger <raphaelmb@gmail.com>
2023-06-06 20:46:24 -0300

    job manager pt1

--- application/services/job_manager.go
+++ application/services/job_manager.go
@@ -1,1 +1,86 @@
 package services
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/jinzhu/gorm"
+	"github.com/raphaelmb/go-ms-encoder/application/repositories"
+	"github.com/raphaelmb/go-ms-encoder/domain"
+	"github.com/raphaelmb/go-ms-encoder/framework/queue"
+	"github.com/streadway/amqp"
+)
+
+type JobManager struct {
+	Db               *gorm.DB
+	Domain           domain.Job
+	MessageChannel   chan amqp.Delivery
+	JobReturnChannel chan JobWorkerResult
+	RabbitMQ         *queue.RabbitMQ
+}
+
+type JobNotificationError struct {
+	Message string `json:"message"`
+	Error   string `json:"error"`
+}
+
+func NewJobManager(db *gorm.DB, rabbitMQ *queue.RabbitMQ, jobReturnChannel chan JobWorkerResult, messageChannel chan amqp.Delivery) *JobManager {
+	return &JobManager{
+		Db:               db,
+		Domain:           domain.Job{},
+		MessageChannel:   messageChannel,
+		JobReturnChannel: jobReturnChannel,
+		RabbitMQ:         rabbitMQ,
+	}
+}
+
+func (j *JobManager) Start(ch *amqp.Channel) {
+	videoService := NewVideoService()
+	videoService.VideoRepository = &repositories.VideoRepositoryDb{Db: j.Db}
+
+	jobService := JobService{
+		JobRepository: &repositories.JobRepostoryDb{Db: j.Db},
+		VideoService:  videoService,
+	}
+
+	concurrency, err := strconv.Atoi(os.Getenv("CONCURRENCY_WORKERS"))
+	if err != nil {
+		log.Fatalf("error loading CONCURRENCY_WORKERS var")
+	}
+
+	for qtdProcesses := 0; qtdProcesses < concurrency; qtdProcesses++ {
+		go JobWorker(j.MessageChannel, j.JobReturnChannel, jobService, j.Domain, qtdProcesses)
+	}
+
+	for jobResult := range j.JobReturnChannel {
+		if jobResult.Error != nil {
+			err = j.checkParseErrors(jobResult)
+		} else {
+			// err = j.notifySuccess(jobResult, ch)
+		}
+
+		if err != nil {
+			jobResult.Message.Reject(false)
+		}
+	}
+}
+
+func (j *JobManager) checkParseErrors(jobResult JobWorkerResult) error {
+	if jobResult.Job.ID != "" {
+		log.Printf("MessageID #{jobResult.Message.DeliveryTag}. Error parsing job: #{jobResult.Job.ID}")
+	} else {
+		log.Printf("MessageID #{jobResult.Message.DeliveryTag}. Error parsing message: #{jobResult.Error}")
+	}
+
+	// errorMessage := JobNotificationError{
+	// 	Message: string(jobResult.Message.Body),
+	// 	Error:   jobResult.Error.Error(),
+	// }
+
+	// jobJson, err := json.Marshal(errorMessage)
+
+	// TODO: notification
+
+	return nil
+}
